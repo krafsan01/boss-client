4db6f00e579f7c17f98022f3034edaa74b2d6901
krafsan01 <krafsan01@gmail.com>
2023-07-13 08:10:11 +0600

    This is a new commit for what I originally planned to be amended

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Home/Home/Shared/Footer/Footer";
+import NavBar from "../Pages/Home/Home/Shared/NavBar/NavBar";
+
+const Main = () => {
+  return (
+    <div>
+      <NavBar></NavBar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Main;
